f2bedda764a74c93417715355a73770a8f287b04
cvalencia1991 <cesar4a6z@gmail.com>
2022-12-20 15:33:32 -0500

    Update Person

--- person.rb
+++ person.rb
@@ -1,8 +1,10 @@
 require './nameable'
+require './capitalize_decorator'
+require './trimmer_decorator'
 class Person < Nameable
   def initialize(age, name = 'Unknown', parent_permission: true)
     super()
-    @id = Random.range(1..100)
+    @id = Random.rand(1..100)
     @name = name
     @age = age
     @parent_permission = parent_permission
@@ -29,3 +31,10 @@ class Person < Nameable
     @name.to_s
   end
 end
+
+person = Person.new(22, 'maximilianus')
+person.correct_name
+capitalizedperson = CapitalizeDecorator.new(person)
+capitalizedperson.correct_name
+capitalizedtrimmedperson = Trimmerdecorator.new(capitalizedperson)
+capitalizedtrimmedperson.correct_name
