1c9d1b2ac085bf1febbf490195f31a38cfa46f03
cvalencia1991 <cesar4a6z@gmail.com>
2022-12-21 13:44:05 -0500

    implement explicit relation in student

--- student.rb
+++ student.rb
@@ -4,6 +4,8 @@ require './classroom'
 class Student < Person
   attr_accessor :classroom
 
+  classroom.students.push(self) unless classroom.students.include?(self)
+
   def initialize(*person, classroom)
     super(*person)
     @classroom = classroom
