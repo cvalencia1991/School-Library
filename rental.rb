e9241955154f50f577c9dc1828881bf9746d9378
cvalencia1991 <cesar4a6z@gmail.com>
2022-12-21 13:43:51 -0500

    Implement relations in rental

--- rental.rb
+++ rental.rb
@@ -1,11 +1,15 @@
 require './book'
 require './person'
 
-class Rental
+class Rentals
   attr_accessor :date, :book, :person
 
   def initialize(date, person)
     @date = date
     @person = person
   end
+
+  book.rentals << self
+  person.rentals << self
+
 end
