267c491a1be7a95a2fcb78aafdd93ea57cfbae20
cvalencia1991 <cesar4a6z@gmail.com>
2022-12-23 08:00:38 -0500

    adding students into array

--- classroom.rb
+++ classroom.rb
@@ -1,15 +1,15 @@
 require './student'
-
 class Classroom
   attr_accessor :label
+  attr_reader :students
 
   def initialize(label)
     @label = label
-    @student = []
+    @students = []
   end
 
-  def add_student(student)
-    @student.push(student)
+  def add_students(student)
+    @students.push(student)
     student.classroom = self
   end
 end
